cdb44ee05940ef948b999caedc7a4cc7ae135889
juanpablo-code2003 <juanpis2140@gmail.com>
2021-06-10 15:22:39 -0500

    Correccion en responsive de terminos y condiciones

--- app/Views/terminos_condiciones.php
+++ app/Views/terminos_condiciones.php
@@ -30,48 +30,41 @@
 		<main class="container-fluid" style="background: #D3D0CB ;min-height: 87vh;">
             <section class="container">
                 <div>
-                    <h1 class="text-center">Terminos y Condiciones MARCHA</h1>
+                    <h1 class="text-center">Términos y Condiciones MARCHA</h1>
                 </div>
                 <div class="card">
-                    <h5 class="card-header">Terminos y Condiciones</h5>
+                    <h5 class="card-header">Términos y Condiciones</h5>
                     <div class="card-body">
                         <div class="row overflow-auto" style="height: 60vh;">
-                            <div class="col-md-10 col-lg-10 col-sm-12">
-                                <div>
-                                    <p>El acceso a este sistema y la utilización de la clave autorizada, con llevan la aceptación del usuario a todas las estipulaciones de este documento y las condiciones aplicables. Este documento constituye un acuerdo para el acceso al sistema de <strong>MARCHA</strong> (Manejo Responsable Campestre de Hogares Agropecuarios), entre el paciente, intermediario o usuario, en adelante denominado el USUARIO que accede a este servicio. </p>
+                            <p style="text-align:justify;">El acceso a este sistema y la utilización de la clave autorizada, con llevan la aceptación del usuario a todas las estipulaciones de este documento y las condiciones aplicables. Este documento constituye un acuerdo para el acceso al sistema de <strong>MARCHA</strong> (Manejo Responsable Campestre de Hogares Agropecuarios), entre el paciente, intermediario o usuario, en adelante denominado el USUARIO que accede a este servicio. </p>
 
-                                    <h4>Advertencia</h4>
-                                    <p> Los términos y condiciones aquí contenidos serán de obligatorio cumplimiento y aceptación por parte de quienes accedan a la información o realicen consulta de resultados que <strong>MARCHA</strong> tiene disponibles. Por favor lea estos términos y condiciones cuidadosamente. Al acceder a este sistema de operación y a cualquiera de sus páginas, el USUARIO acepta los términos y condiciones que aparecen a continuación, así como también aquellas obligaciones que surgen de la naturaleza de la herramienta tecnológica que se está utilizando y de la buena fe que impone el ordenamiento jurídico colombiano. Si no está de acuerdo con los términos y condiciones a continuación presentados, NO debe continuar y debe SALIR del sistema.</p>
+                            <h4>Advertencia</h4>
+                            <p style="text-align:justify;"> Los términos y condiciones aquí contenidos serán de obligatorio cumplimiento y aceptación por parte de quienes accedan a la información o realicen consulta de resultados que <strong>MARCHA</strong> tiene disponibles. Por favor lea estos términos y condiciones cuidadosamente. Al acceder a este sistema de operación y a cualquiera de sus páginas, el USUARIO acepta los términos y condiciones que aparecen a continuación, así como también aquellas obligaciones que surgen de la naturaleza de la herramienta tecnológica que se está utilizando y de la buena fe que impone el ordenamiento jurídico colombiano. Si no está de acuerdo con los términos y condiciones a continuación presentados, NO debe continuar y debe SALIR del sistema.</p>
 
-                                    <h4>Propiedad de la información</h4>
-                                    <p><strong>MARCHA</strong> es el titular de los derechos de autor y propiedad intelectual sobre los signos distintivos en general y marcas comerciales, contenidos en estas páginas. Los derechos de autor sobre las páginas, las imágenes y en general todo lo que aparece en pantalla, así como sobre la información y el material contenido en las mismas, es propiedad de <strong>MARCHA</strong> salvo indicación en contrario. </p>
+                            <h4>Propiedad de la información</h4>
+                            <p style="text-align:justify;"><strong>MARCHA</strong> es el titular de los derechos de autor y propiedad intelectual sobre los signos distintivos en general y marcas comerciales, contenidos en estas páginas. Los derechos de autor sobre las páginas, las imágenes y en general todo lo que aparece en pantalla, así como sobre la información y el material contenido en las mismas, es propiedad de <strong>MARCHA</strong> salvo indicación en contrario. </p>
 
-                                    <h4>Consultas y Operaciones habilitadas</h4>
-                                    <p>El usuario mediante la utilización de este sistema podrá realizar el manejo completo del CRUD en diferentes campos de informacion.</p>
+                            <h4>Consultas y Operaciones habilitadas</h4>
+                            <p style="text-align:justify;">El usuario mediante la utilización de este sistema podrá realizar el manejo completo del CRUD en diferentes campos de informacion.</p>
 
-                                    <h4>Alcance de la información</h4>
-                                    <p><strong>MARCHA</strong> ha dispuesto sus mejores esfuerzos para que la información y los materiales aquí contenidos, incluyendo los textos, gráficos, enlaces y demás
-                                    ítems, sean idóneos para la correcta operación por parte del USUARIO. Sin embargo, MARCHA no asumirá pagos por concepto de daños directos, indirectos, accidentales,
-                                    consecuenciales, punitivos, incluyendo pérdidas y lucro cesante que subjetivamente se imputen a: (i) la información contenida en la Página Web de <strong>MARCHA</strong>, (ii) cualquier información transmitida vía la Página Web de NOMBRE DE LA EMPRESA o (iii) el funcionamiento o no de cualquier Página Web de <strong>MARCHA</strong>. <strong>MARCHA</strong> no asumirá monto alguno por daños o pérdidas de equipo o software que se imputen subjetivamente a virus, defectos o mal funcionamiento relativos a accesos o utilización de la Página Web <strong>MARCHA</strong>.</p>
+                            <h4>Alcance de la información</h4>
+                            <p style="text-align:justify;"><strong>MARCHA</strong> ha dispuesto sus mejores esfuerzos para que la información y los materiales aquí contenidos, incluyendo los textos, gráficos, enlaces y demás
+                            ítems, sean idóneos para la correcta operación por parte del USUARIO. Sin embargo, MARCHA no asumirá pagos por concepto de daños directos, indirectos, accidentales, consecuenciales, punitivos, incluyendo pérdidas y lucro cesante que subjetivamente se imputen a: (i) la información contenida en la Página Web de <strong>MARCHA</strong>, (ii) cualquier información transmitida vía la Página Web de <strong>MARCHA</strong> o (iii) el funcionamiento o no de cualquier Página Web de <strong>MARCHA</strong>. <strong>MARCHA</strong> no asumirá monto alguno por daños o pérdidas de equipo o software que se imputen subjetivamente a virus, defectos o mal funcionamiento relativos a accesos o utilización de la Página Web <strong>MARCHA</strong>.</p>
 
-                                    <h4>Acceso al sistema, código de acceso, contraseña y seguridad</h4>
-                                    <p>El USUARIO será responsable por la confidencialidad y uso de su número de acceso, contraseña y número de cuenta. El USUARIO será responsable por todas las órdenes transmitidas a través de su número de acceso, contraseña, y cualquier orden recibida por <strong>MARCHA</strong> de esta manera se supondrá emitida por el USUARIO. Todas las órdenes emitidas, se supondrán realizadas en el momento en que sean recibidas por <strong>MARCHA</strong>, salvo que el reglamento o las normas legales que regulan cada producto o servicio, dispongan otra cosa. El USUARIO acepta que notificará a <strong>MARCHA</strong> cualquiera de las siguientes situaciones: *Pérdida o robo de su código de acceso, contraseña y/o número de cuenta. *Uso no autorizado de su código de acceso o contraseña. * Alguna falla, error o hecho inusual, al recibir algún mensaje relacionado con una orden iniciada por el USUARIO a través del sistema electrónico, o que haya sido recibida y/o ejecutada a través del mismo. * La confirmación de alguna orden que el USUARIO no emitió, o alguna imprecisión o desacuerdo en la transmisión de la información.</p>
+                            <h4>Acceso al sistema, código de acceso, contraseña y seguridad</h4>
+                            <p style="text-align:justify;">El USUARIO será responsable por la confidencialidad y uso de su número de acceso, contraseña y número de cuenta. El USUARIO será responsable por todas las órdenes transmitidas a través de su número de acceso, contraseña, y cualquier orden recibida por <strong>MARCHA</strong> de esta manera se supondrá emitida por el USUARIO. Todas las órdenes emitidas, se supondrán realizadas en el momento en que sean recibidas por <strong>MARCHA</strong>, salvo que el reglamento o las normas legales que regulan cada producto o servicio, dispongan otra cosa. El USUARIO acepta que notificará a <strong>MARCHA</strong> cualquiera de las siguientes situaciones: *Pérdida o robo de su código de acceso, contraseña y/o número de cuenta. *Uso no autorizado de su código de acceso o contraseña. * Alguna falla, error o hecho inusual, al recibir algún mensaje relacionado con una orden iniciada por el USUARIO a través del sistema electrónico, o que haya sido recibida y/o ejecutada a través del mismo. * La confirmación de alguna orden que el USUARIO no emitió, o alguna imprecisión o desacuerdo en la transmisión de la información.</p>
 
-                                    <h4>Transmisión de datos</h4>
-                                    <p><strong>MARCHA</strong> ha dispuesto de una organización tecnológica en procura de que el usuario pueda efectuar la transmisión de datos a través de Internet, o utilizando otro tipo de comunicación en red, o cualquier otra vía electrónica. Antes de suministrar cualquier información el USUARIO deberá autorizar a <strong>MARCHA</strong> para almacenar y tratar sus datos. Dichos datos serán tratados con altas medidas de confidencialidad.</p>
+                            <h4>Transmisión de datos</h4>
+                            <p style="text-align:justify;"><strong>MARCHA</strong> ha dispuesto de una organización tecnológica en procura de que el usuario pueda efectuar la transmisión de datos a través de Internet, o utilizando otro tipo de comunicación en red, o cualquier otra vía electrónica. Antes de suministrar cualquier información el USUARIO deberá autorizar a <strong>MARCHA</strong> para almacenar y tratar sus datos. Dichos datos serán tratados con altas medidas de confidencialidad.</p>
 
-                                    <h4>Terminación del acceso</h4>
-                                    <p><strong>MARCHA</strong> se reserva el derecho de cancelar unilateralmente el acceso a este sistema de operación y transacción en cualquier tiempo, sin que para ello tenga que aducir alguna justa causa. La cancelación del acceso no implica la terminación de las relaciones contractuales que tenga el USUARIO a <strong>MARCHA</strong>.</p>
+                            <h4>Terminación del acceso</h4>
+                            <p style="text-align:justify;"><strong>MARCHA</strong> se reserva el derecho de cancelar unilateralmente el acceso a este sistema de operación y transacción en cualquier tiempo, sin que para ello tenga que aducir alguna justa causa. La cancelación del acceso no implica la terminación de las relaciones contractuales que tenga el USUARIO a <strong>MARCHA</strong>.</p>
 
-                                    <h4>Modificaciones y revisiones al presente acuerdo</h4>
-                                    <p><strong>MARCHA</strong> se reserva el derecho de revisar el presente acuerdo en cualquier momento y de modificarlo sin previo aviso.</p>
+                            <h4>Modificaciones y revisiones al presente acuerdo</h4>
+                            <p style="text-align:justify;"><strong>MARCHA</strong> se reserva el derecho de revisar el presente acuerdo en cualquier momento y de modificarlo sin previo aviso.</p>
 
-                                    <h4>Aceptación</h4>
-                                    <p> El acceso a este sistema y la utilización de la clave autorizada, conllevan la aceptación del usuario a todas las estipulaciones de este acuerdo.</p>
-                                    
-                                </div>    
-                            </div>
-                            
+                            <h4>Aceptación</h4>
+                            <p style="text-align:justify;"> El acceso a este sistema y la utilización de la clave autorizada, conllevan la aceptación del usuario a todas las estipulaciones de este acuerdo.</p>
                         </div>
                     </div>
                 </div>
